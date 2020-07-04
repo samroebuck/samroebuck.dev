f9e51548a5c14ae2436762db3f6419af539599fb
Sam Roebuck <samiiroebuck.96@gmail.com>
2020-07-04 18:40:16 +0100

    alter location component for reuse as company as well

--- src/components/sidebar/location.js
+++ src/components/sidebar/location.js
@@ -1,15 +1,19 @@
 import React from "react"
 import { FaMapMarkerAlt } from "react-icons/fa"
+import { FaBriefcase } from "react-icons/fa"
+
 import { ProfileType } from "../../types"
 
-const Location = ({ location, relocation }) => (
+const Location = ({ location, relocation, company }) => (
   <>
-    <div className="inline-flex items-center bg-front mt-6 w-auto">
-      <span className="text-lead border-r-2 border-back px-3">
-        <FaMapMarkerAlt className="h-4 w-4" />
+    <div className="inline-flex items-center bg-front mt-3 min-w-full">
+      <span className="text-lead-text border-r-2 border-back px-3">
+      {location ? <FaMapMarkerAlt className="h-4 w-4" /> : null}
+      {company ? <FaBriefcase className="h-4 w-4" /> : null}
       </span>
       <span className="text-back font-header font-bold py-1 px-3 text-lg">
-        {location}
+      {location ? location : null}
+      {company ? company : null}
       </span>
     </div>
     {relocation && (
