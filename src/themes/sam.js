c21de32e9528e031cecdb9acf39096d63b667a34
Sam Roebuck <samiiroebuck.96@gmail.com>
2020-07-04 18:38:17 +0100

    add custom theme

---
+++ src/themes/sam.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'back': '#F8F8F8',
+    'front': '#40324C',
+    'lead': '#40324C',
+    'pink': '#CE7E8F',
+    'lead-text': '#ffffff',
+    'line': '#7E6671',
+    'skill-1': '#CE7E8F',
+    'skill-2': '#CE7E8F',
+    'skill-3': '#CE7E8F',
+  };
