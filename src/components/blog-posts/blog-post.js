832489bc0c98d5df8c72b59e1445fc87ddc9dd0b
Sam Roebuck <samiiroebuck.96@gmail.com>
2021-01-24 18:18:48 +0000

    move title link and change blog item border colour

--- src/components/blog-posts/blog-post.js
+++ src/components/blog-posts/blog-post.js
@@ -3,9 +3,8 @@ import React from 'react';
 const BlogPost = (props) => {
   const { title, url, description } = props
   return (
-    <article className="border-t-4 border-line relative flex flex-wrap bg-back-light p-4 lg:p-8 bg-no-repeat text-md mb-6">
+    <article className="border-t-4 border-pink relative flex flex-wrap bg-back-light p-4 lg:p-8 bg-no-repeat text-md mb-6">
       <div className="lg:flex-1">
-        <h3 className="font-bold">{title}</h3>
         {url && (
           <a
             className="text-front underline break-all hover:opacity-75 transition-opacity duration-150"
@@ -13,7 +12,7 @@ const BlogPost = (props) => {
             rel="noreferrer noopener"
             target="_blank"
           >
-            {url}
+            <h3 className="font-bold">{title}</h3>
           </a>
         )}
         <p className="w-full py-4 whitespace-pre-line">{description}</p>
--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -9,7 +9,6 @@ const Project = props => {
   return (
     <article className="border-t-4 border-line relative flex flex-wrap bg-back-light p-4 lg:p-8 bg-no-repeat text-md mb-6">
       <div className="lg:flex-1">
-        <h3 className="font-bold">{name}</h3>
         {url && (
           <a
             className="text-front underline break-all hover:opacity-75 transition-opacity duration-150"
@@ -17,7 +16,7 @@ const Project = props => {
             rel="noreferrer noopener"
             target="_blank"
           >
-            {url}
+            <h3 className="font-bold">{name}</h3>
           </a>
         )}
         <p className="w-full py-4 whitespace-pre-line">{description}</p>
