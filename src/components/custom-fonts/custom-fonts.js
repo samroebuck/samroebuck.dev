67e0d543855810e3920eef3d4805662febb84722
Sam Roebuck <samiiroebuck.96@gmail.com>
2021-01-24 17:30:09 +0000

    preload font

--- src/components/custom-fonts/custom-fonts.js
+++ src/components/custom-fonts/custom-fonts.js
@@ -4,9 +4,18 @@ import { Helmet } from "react-helmet"
 const CustomFonts = () => (
   <Helmet>
     <link
-      rel="stylesheet"
+      rel="preload"
       href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Source+Sans+Pro:wght@300;400;600;700;900&display=swap"
+      as="style"
+      onload="this.onload=null;this.rel='stylesheet'"
     />
+    <noscript>
+      {`<link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Source+Sans+Pro:wght@300;400;600;700;900&display=swap"
+          rel="stylesheet"
+          type="text/css"
+      />`}
+    </noscript>
   </Helmet>
 )
 
