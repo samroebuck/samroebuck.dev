df9ef69263ba07a81bff7d46769287f1caf23bf3
Sam Roebuck <samiiroebuck.96@gmail.com>
2022-12-29 20:54:02 +0000

    amend image usage

--- components/sidebar/profile-image.tsx
+++ components/sidebar/profile-image.tsx
@@ -4,9 +4,12 @@ const ProfileImage = () => (
   <div className="relative inline-block leading-none">
   <div className="relative z-10 diamond">
     <Image
-      src="profile.JPG"
+      src="/profile.JPG"
       alt='Sam Roebuck'
-      className="relative z-10 diamond-img"></Image>
+      height={200}
+      width={200}
+      className="relative z-10 diamond-img"
+></Image>
     </div>
     <div className="absolute inset-0 diamond diamond-off bg-pink z-0 transform translate-x-12"></div>
   </div>
